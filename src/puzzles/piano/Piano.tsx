fc2bf5364ff00a6d96ec0f46fcb7d908f51d704e
ib-rim <ib.bashir@hotmail.com>
2023-06-24 21:25:59 +0100

    Add basic piano puzzle functionality

---
+++ src/puzzles/piano/Piano.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import './Piano.scss';
+
+export default function Piano() {
+
+    const [password] = useState<string>("DEAFBED");
+    const [guess, setGuess] = useState<string | undefined>("");
+
+    const handleKeyPress = (note: string) => {
+        let currentGuess: string = guess + note;
+        if (password.slice(0, currentGuess.length) === currentGuess) {
+            if (password === currentGuess) {
+                console.log("WINNER");
+            }
+            setGuess(currentGuess);
+        }
+        else {
+            if (currentGuess === "DD") {
+                setGuess("D");
+            }
+            else {
+                setGuess("");
+            }
+        }
+    }
+
+    return (
+        <>
+            <div className="piano">
+                <div className="scale">
+                    <button onClick={() => handleKeyPress("C")} className="piano-key" />
+                    <button onClick={() => handleKeyPress(" ")} className="piano-key piano-key--black" />
+                    <button onClick={() => handleKeyPress("D")} className="piano-key" />
+                    <button onClick={() => handleKeyPress(" ")} className="piano-key piano-key--black" />
+                    <button onClick={() => handleKeyPress("E")} className="piano-key" />
+                    <button onClick={() => handleKeyPress("F")} className="piano-key" />
+                    <button onClick={() => handleKeyPress(" ")} className="piano-key piano-key--black" />
+                    <button onClick={() => handleKeyPress("G")} className="piano-key" />
+                    <button onClick={() => handleKeyPress(" ")} className="piano-key piano-key--black" />
+                    <button onClick={() => handleKeyPress("A")} className="piano-key" />
+                    <button onClick={() => handleKeyPress(" ")} className="piano-key piano-key--black" />
+                    <button onClick={() => handleKeyPress("B")} className="piano-key" />
+                </div>
+                <div className="scale">
+                    <button onClick={() => handleKeyPress("C")} className="piano-key" />
+                    <button onClick={() => handleKeyPress(" ")} className="piano-key piano-key--black" />
+                    <button onClick={() => handleKeyPress("D")} className="piano-key" />
+                    <button onClick={() => handleKeyPress(" ")} className="piano-key piano-key--black" />
+                    <button onClick={() => handleKeyPress("E")} className="piano-key" />
+                    <button onClick={() => handleKeyPress("F")} className="piano-key" />
+                    <button onClick={() => handleKeyPress(" ")} className="piano-key piano-key--black" />
+                    <button onClick={() => handleKeyPress("G")} className="piano-key" />
+                    <button onClick={() => handleKeyPress(" ")} className="piano-key piano-key--black" />
+                    <button onClick={() => handleKeyPress("A")} className="piano-key" />
+                    <button onClick={() => handleKeyPress(" ")} className="piano-key piano-key--black" />
+                    <button onClick={() => handleKeyPress("B")} className="piano-key" />
+                </div>
+            </div>
+        </>
+    )
+}
