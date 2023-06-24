c88633e25454f7a021931a53e6631c3d56a490ff
ib-rim <ib.bashir@hotmail.com>
2023-06-24 21:26:19 +0100

    Add Piano component to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Piano from './puzzles/piano/Piano';
 
 function App() {
     return (
         <div className="App">
-            
+            <Piano />
         </div>
     );
 }
