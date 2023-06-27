d32da69f866f2c1d0e6834df539c7ccc4976c44a
ib-rim <ib.bashir@hotmail.com>
2023-06-27 17:58:08 +0100

    Add mouth component

---
+++ src/puzzles/eating/Eating.tsx
@@ -0,0 +1,28 @@
+import './Eating.scss'
+
+export default function Eating() {
+    return (
+        <div className="mouth">
+            <div className="teeth teeth--top">
+                <div className="tooth" />
+                <div className="tooth" />
+                <div className="tooth" />
+                <div className="tooth" />
+                <div className="tooth" />
+                <div className="tooth" />
+                <div className="tooth" />
+                <div className="tooth" />
+            </div>
+            <div className="teeth teeth--bottom">
+                <div className="tooth" />
+                <div className="tooth" />
+                <div className="tooth" />
+                <div className="tooth" />
+                <div className="tooth" />
+                <div className="tooth" />
+                <div className="tooth" />
+                <div className="tooth" />
+            </div>
+        </div>
+    )
+}
