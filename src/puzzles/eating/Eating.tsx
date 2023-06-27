d6b1700e62e11c8c0b0f9af0890cdfcb23db4817
ib-rim <ib.bashir@hotmail.com>
2023-06-27 19:38:20 +0100

    Add cursor follow and chomp animation

--- src/puzzles/eating/Eating.tsx
+++ src/puzzles/eating/Eating.tsx
@@ -1,27 +1,63 @@
+import { useEffect, useRef, useState } from 'react'
 import './Eating.scss'
+import { toContainHTML } from '@testing-library/jest-dom/matchers'
 
 export default function Eating() {
+
+    const [mousePosition, setMousePosition] = useState<{ [pos: string]: Number }>({
+        "x": 0,
+        "y": 0
+    })
+    const [toothAnimation, setToothAnimation] = useState<string | undefined>('');
+
+    const mouthElem = useRef<HTMLInputElement | null>(null)
+
+    const mouthChomp = () => {
+        setToothAnimation("chompAnimation");
+        setTimeout(() => {
+            setToothAnimation("");
+        }, 1000)
+    }
+
+    const handleMouseMove = (e: MouseEvent) => {
+        setMousePosition({
+            "x": e.pageX,
+            "y": e.pageY
+        });
+        if (mouthElem.current) {
+            mouthElem.current.style.left = mousePosition.x.toString() + "px";
+            mouthElem.current.style.top = mousePosition.y.toString() + "px";
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener("mousemove", handleMouseMove);
+        return () => document.removeEventListener("mousemove", handleMouseMove);
+    })
+
     return (
-        <div className="mouth">
-            <div className="teeth teeth--top">
-                <div className="tooth" />
-                <div className="tooth" />
-                <div className="tooth" />
-                <div className="tooth" />
-                <div className="tooth" />
-                <div className="tooth" />
-                <div className="tooth" />
-                <div className="tooth" />
-            </div>
-            <div className="teeth teeth--bottom">
-                <div className="tooth" />
-                <div className="tooth" />
-                <div className="tooth" />
-                <div className="tooth" />
-                <div className="tooth" />
-                <div className="tooth" />
-                <div className="tooth" />
-                <div className="tooth" />
+        <div className="room" onClick={() => mouthChomp()}>
+            <div ref={mouthElem} className="mouth">
+                <div className="teeth teeth--top">
+                    <div className={`tooth ${toothAnimation}`} />
+                    <div className={`tooth ${toothAnimation}`} />
+                    <div className={`tooth ${toothAnimation}`} />
+                    <div className={`tooth ${toothAnimation}`} />
+                    <div className={`tooth ${toothAnimation}`} />
+                    <div className={`tooth ${toothAnimation}`} />
+                    <div className={`tooth ${toothAnimation}`} />
+                    <div className={`tooth ${toothAnimation}`} />
+                </div>
+                <div className="teeth teeth--bottom">
+                    <div className={`tooth ${toothAnimation}`} />
+                    <div className={`tooth ${toothAnimation}`} />
+                    <div className={`tooth ${toothAnimation}`} />
+                    <div className={`tooth ${toothAnimation}`} />
+                    <div className={`tooth ${toothAnimation}`} />
+                    <div className={`tooth ${toothAnimation}`} />
+                    <div className={`tooth ${toothAnimation}`} />
+                    <div className={`tooth ${toothAnimation}`} />
+                </div>
             </div>
         </div>
     )
